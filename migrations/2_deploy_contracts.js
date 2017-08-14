8c7ac2c0ae23a575d2632d3a6103d843068a3b22
Andrey Degtyaruk <hlogeon1@gmail.com>
2017-08-14 19:02:34 +0300

    Set Soft cap to 150000$

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -8,7 +8,7 @@ module.exports = function(deployer) {
   deployer.link(SafeMath, JincorTokenPreSale);
   deployer.deploy(JincorToken).then(function() {
     const hardCap = web3.toWei(1372, "ether") * 255; //in USD
-    const softCap = 100;
+    const softCap = 150000;
     const price = web3.toWei(0.000980392158, "ether") * 255; //in USD
     const token = JincorToken.address;
     const totalTokens = web3.toWei(1400000, "ether");
